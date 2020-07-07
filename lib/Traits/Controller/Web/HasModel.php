949c8d25ae115f05d3ff171431f8ef0a48e11de3
Lonnie Blansett <lonnie@limbonia.tech>
2020-07-07 13:23:30 -0500

    Updated naming convention

--- lib/Traits/Controller/Web/HasModel.php
+++ lib/Traits/Controller/Web/HasModel.php
@@ -1,5 +1,5 @@
 <?php
-namespace Limbonia\Traits\Controller;
+namespace Limbonia\Traits\Controller\Web;
 
 /**
  * Limbonia ModelController Trait
@@ -9,7 +9,7 @@ namespace Limbonia\Traits\Controller;
  * @author Lonnie Blansett <lonnie@limbonia.tech>
  * @package Limbonia
  */
-trait WebModel
+trait HasModel
 {
   /**
    * Initialize this controller's custom data, if there is any
